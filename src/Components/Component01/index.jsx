56626ce04fc513126e99617b70481a5642c39fd1
Joanderson SIlva <jhonny_040996@hotmail.com>
2022-07-08 10:24:59 -0300

    first commit

---
+++ src/Components/Component01/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+export function Component01(){
+   return(
+    <>
+      
+    </>
+   )
+}
---
+++ src/Components/Component02/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+export function Component02(){
+   return(
+    <>
+      
+    </>
+   )
+}
